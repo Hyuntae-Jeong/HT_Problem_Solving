03bb2a23573e17f9d5dd0a796692ef2f4b29ae39
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2025-08-26 21:33:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 480 ms, Memory: 47220 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,84 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Main {
+    static int N, SIZE = 200001;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Node xHeads[] = new Node[SIZE];    // original value + 100000 = index
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        startProcess();
+        printNodes();
+    }
+
+    public static void startProcess() throws IOException {
+        StringTokenizer token;
+        int x, y;
+
+        for (int i = 0; i < N; i++) {
+            token = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(token.nextToken());
+            y = Integer.parseInt(token.nextToken());
+
+            insertNode(x + (SIZE-1)/2, y);
+        }
+    }
+
+    public static void insertNode(int x, int y) {
+        if (xHeads[x] == null) {
+            xHeads[x] = new Node(y);
+            return;
+        }
+
+        Node currNode = xHeads[x];
+        Node newNode = new Node(y);
+
+        // case 1: head보다 작은 경우
+        if (y < xHeads[x].value){
+            newNode.next = xHeads[x];
+            xHeads[x] = newNode;
+            return;
+        }
+
+        // case 2: head보단 큰 경우
+        while(currNode.next != null) {
+            if (y < currNode.next.value) break;
+            currNode = currNode.next;
+        }
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+    }
+
+    public static void printNodes() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < SIZE; i++) {
+            if (xHeads[i] == null) continue;
+
+            Node currNode = xHeads[i];
+            while(currNode != null) {
+                sb.append(i - (SIZE-1)/2).append(" ").append(currNode.value).append("\n");
+                currNode = currNode.next;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
