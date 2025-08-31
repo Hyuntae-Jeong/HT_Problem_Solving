cbf34a52ce36c1826ba8be312b1a90eff5b1e809
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2025-08-31 20:37:42 +0900

    [Silver IV] Title: ATM, Time: 80 ms, Memory: 12048 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static PriorityQueue<Integer> waitTimes;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        waitTimes = new PriorityQueue<>();
+
+        getInputs();
+        calculateMinTimes();
+    }
+
+    static void getInputs() throws IOException {
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            waitTimes.add(Integer.parseInt(token.nextToken()));
+        }
+    }
+
+    static void calculateMinTimes() {
+        Integer time;
+        int totalTime = 0;
+
+        while (N > 0){
+            time = waitTimes.poll();
+            if (time == null) break;
+
+            totalTime += time * N--;
+        }
+
+        System.out.println(totalTime);
+    }
+}
