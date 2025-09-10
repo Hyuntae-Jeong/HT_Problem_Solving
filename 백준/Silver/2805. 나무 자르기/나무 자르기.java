07901cbaeaea4bf0ff301d5f70b74181061e5e3b
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2025-09-11 01:24:00 +0900

    [Silver II] Title: 나무 자르기, Time: 436 ms, Memory: 167804 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -24,16 +23,15 @@ class Main {
             trees[i] = Integer.parseInt(token.nextToken());
             if (trees[i] > max) max = trees[i];
         }
-//        System.out.printf("min: %d , max: %d\n", min, max);
+        
         System.out.println(binarySearch());
     }
 
     static int binarySearch () {
 
         while (true) {
-            double sumTree = 0; int height = (min + max) / 2;
-
-//            System.out.println("height is " + height);
+            double sumTree = 0;             // 더하다 보면 정수 범위를 넘어서는 경우 존재하므로 double로 선언
+            int height = (min + max) / 2;
 
             for (int i = 0; i < numTree; i++) {
                 if (trees[i] > height) sumTree += (trees[i] - height);
@@ -53,13 +51,7 @@ class Main {
                 return height;
             }
 
-
             if (max < min) return maxHeight;
-            // todo: min 3, max 4 경합이 이루어지는 경우
-            // min이 3이다 -> 3 위치에서는 목표치보다 양이 많아서 올려도 된다
-            // max가 4이다 -> 4 위치에서는 목표치보다 양이 부족하다
-            // 이때 height는 3이 되고 올려야 하므로 min이 또 3이 돼서 무한 반복... => 3이 최선이므로 이때는 3 (maxHeight) 리턴하도록..
-            // if 조건을 min >= max 에서 max - min <= 1 로 변경
         }
     }
 }
