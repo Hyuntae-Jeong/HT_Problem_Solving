d23aec88ad344e793cd7a65f4a9b12db5d9012b4
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2025-11-03 00:41:13 +0900

    [Bronze V] Title: N 찍기, Time: 152 ms, Memory: 17132 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
