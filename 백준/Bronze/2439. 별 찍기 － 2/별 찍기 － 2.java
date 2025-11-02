0b1733b1abf3ba188f71db5d029bbd62f418df5f
Hyuntae Jeong <63701384+Hyuntae-Jeong@users.noreply.github.com>
2025-11-03 00:25:02 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 68 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N ;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        printStars();
+    }
+
+    static void printStars() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) sb.append(" ");
+            for (int j = 1; j <= i; j++) sb.append("*");
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
