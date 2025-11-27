39647adf70a7448cecd3e639c6c9626550a970db
Hyuntae <jht000717@gmail.com>
2025-11-28 00:04:01 +0900

    [Silver IV] Title: solved.ac, Time: 292 ms, Memory: 36644 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[] points;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        points = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            points[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(points);
+
+        int cutline = (int) Math.round((double)N * 0.15);
+        double sum = 0;
+        for (int i = cutline; i < N - cutline; i++) {
+            sum += points[i];
+        }
+
+        System.out.print(Math.round(sum / (N - cutline * 2)));
+    }
+}
