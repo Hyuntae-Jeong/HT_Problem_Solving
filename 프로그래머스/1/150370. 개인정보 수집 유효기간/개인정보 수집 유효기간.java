6621663443132ff2fdf89054ea049cb16b258c3c
Hyuntae <jht000717@gmail.com>
2025-12-03 22:19:59 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 7.86 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,85 +1,49 @@
 import java.util.*;
 import java.util.stream.Collectors;
-
 class Solution {
-    
-    static int[] todayDate;
+    static int todayDate = 0;
     static Map<String, Integer> termMap;
-    
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        convertDataType(today, terms, privacies);
-        
+
+    public static int[] solution(String today, String[] terms, String[] privacies) {
+        convertDataType(today, terms);
         int[] answer = findExpiredPrivacy(privacies);
-        
+        /* for (int i = 0; i < answer.length; i++) {
+            System.out.printf("%d ", answer[i]);
+        } */
         return answer;
     }
-    
-    public void convertDataType(String today, String[] terms, String[] privacies) {
-        // [index] 0: year, 1: month, 2:date
-        todayDate = Arrays.stream(today.split("\\."))
-            .mapToInt(Integer::valueOf)
-            .toArray();
-        
-        // key: 약관 종류, value: 유효기간
-        termMap = Arrays.stream(terms)
-            .map(term -> term.split(" "))
-            .collect(Collectors.toMap(
-                parts -> parts[0],
-                parts -> Integer.parseInt(parts[1])
-            ));
-        
-        /* for (Map.Entry<String, Integer> entry : termMap.entrySet()) {
-            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
-        } */
+
+    static void convertDataType(String today, String[] terms) {
+        // 오늘 날짜를 일수로 변경하기 (한달에 28일 고정)
+        String[] todayValue = today.split("\\.");
+        todayDate += Integer.parseInt(todayValue[0]) * 12 * 28;     // year
+        todayDate += Integer.parseInt(todayValue[1]) * 28;          // month
+        todayDate += Integer.parseInt(todayValue[2]);               // date
+
+        // 약관 유효기간 Map 형태로 변환하기
+        termMap = new HashMap<>(terms.length);
+        for (int i = 0; i < terms.length; i++) {
+            termMap.put(terms[i].split(" ")[0], Integer.valueOf(terms[i].split(" ")[1]));
+        }
     }
-    
-    public int[] findExpiredPrivacy(String[] privacies) {
+
+    static int[] findExpiredPrivacy(String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-        
+
         for (int i = 0; i < privacies.length; i++) {
-            String parts[] = privacies[i].split(" ");
-            
-            int[] date = Arrays.stream(parts[0].split("\\."))
-                .mapToInt(Integer::valueOf)
-                .toArray();
-            
-            // 날짜 계산하기: 먼저 수집 일자에서 하루 뺀 뒤에 유효기간을 뺀다
-            
-            // 일 빼기 (-1 day)
-            date[2] -= 1;
-            if (date[2] <= 0) {
-                date[1] -= 1;
-                date[2] += 28;
-            }
-            
-            /* for (int x = 0; x < 3; x++) {
-                System.out.printf("%d ", date[x]);
-            }
-            System.out.println(); */
-            
-            // 월 더하기
-            date[1] += termMap.get(parts[1]);            
-            while (date[1] > 12) {
-                date[0] += 1;
-                date[1] -= 12;
-            }
-            
-           
-            
-            // 날짜 비교하기: todayDate보다 date가 과거이면 만료된 것
-            if (todayDate[0] > date[0]) {
-                answer.add(i+1);
-            } else if (todayDate[0] == date[0]) {
-                if (todayDate[1] > date[1]) {
-                    answer.add(i+1);
-                } else if (todayDate[1] == date[1]) {
-                    if (todayDate[2] > date[2]) {
-                        answer.add(i+1);
-                    }
-                }
+            String[] dateValue = privacies[i].split(" ")[0].split("\\.");
+            int expireDate = Integer.parseInt(dateValue[0]) * 12 * 28;     // year
+            expireDate += Integer.parseInt(dateValue[1]) * 28;            // month
+            expireDate += Integer.parseInt(dateValue[2]);                 // date
+
+            expireDate -= 1;
+            expireDate += 28 * termMap.get(privacies[i].split(" ")[1]);
+
+            if (todayDate > expireDate) {
+                answer.add(i + 1);
             }
         }
-        
+
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
