71106cc0ae381ae01e274c75f069470d92b78ce9
Hyuntae <jht000717@gmail.com>
2025-12-24 20:19:29 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11528 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static final int r = 31, M = 1234567891;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        String inputString = br.readLine();
+        char[] inputs = inputString.toCharArray();
+
+        int ri = 1, hash = 0;
+        for (char letter : inputs) {
+            int value = Character.getNumericValue(letter) - 9;
+            hash = (hash + value * ri) % M;
+            ri *= r;
+        }
+
+        System.out.println(hash);
+    }
+}
