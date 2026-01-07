133757c8504582cd977e8085079facdeecb92d7c
Hyuntae <jht000717@gmail.com>
2026-01-07 22:39:40 +0900

    [Gold V] Title: 용액, Time: 260 ms, Memory: 32100 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,180 +1,67 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.StringTokenizer;
 
-class Answer {
-    long absValue;
-    long liquid1, liquid2;
+class Mixture {
+    long liquidA, liquidB, valueABS;
 
-    Answer () {
-        this.absValue = Long.MAX_VALUE;
+    Mixture () {
+        liquidA = 0;
+        liquidB = 0;
+        valueABS = Long.MAX_VALUE;
     }
 }
-
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N;
-    static StringTokenizer token;
-    static Answer answer = new Answer();
+    static long[] liquidList;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        token = new StringTokenizer(br.readLine());
 
-        getInputUntilPositiveNum();
-        getInputPositiveNumAndDoBinarySearch();
+        getInput();
+        twoPointerSearch();
     }
 
-    static long[] minus;
-    static int endIndexOfMinus = -1;
-    static long firstPositive = 0, secondPositive = 0;
+    static void getInput() throws IOException {
+        liquidList = new long[N];
+        StringTokenizer token = new StringTokenizer(br.readLine());
 
-    static void getInputUntilPositiveNum() {
-        minus = new long[N];
-        while (token.hasMoreElements()) {
-            long value = Long.parseLong(token.nextToken());
-            if (value > 0) {
-                firstPositive = value;
-                break;
-            } else {
-                minus[++endIndexOfMinus] = value;
-            }
+        for (int i = 0; i < N; i++) {
+            liquidList[i] = Long.parseLong(token.nextToken());
         }
     }
 
-    static void getInputPositiveNumAndDoBinarySearch() {
-        if (firstPositive != 0) {
-            if(binarySearch(firstPositive)){
-                System.out.printf("%d %d", Math.min(answer.liquid1, answer.liquid2), Math.max(answer.liquid1, answer.liquid2));
-                return;
-            }
-        }
+    static void twoPointerSearch() {
+        Mixture mixture = new Mixture();
+        int left = 0, right = N - 1;
 
-        if (token.hasMoreTokens()) {
-            secondPositive = Long.parseLong(token.nextToken());
-            if(binarySearch(secondPositive)){
-                System.out.printf("%d %d", Math.min(answer.liquid1, answer.liquid2), Math.max(answer.liquid1, answer.liquid2));
-                return;
-            }
-        }
-
-        while (token.hasMoreTokens()) {
-            if(binarySearch(Integer.parseInt(token.nextToken()))){
-                System.out.printf("%d %d", Math.min(answer.liquid1, answer.liquid2), Math.max(answer.liquid1, answer.liquid2));
-                return;
-            }
-        }
-
-        getFinalAnswer();
-    }
-
-    static void getFinalAnswer() {
-        // 음수 2개 더한 값의 용액값과 이진값에서 구한 용액값 비교
-        if (endIndexOfMinus >= 1) {
-            if (Math.abs(minus[endIndexOfMinus - 1] + minus[endIndexOfMinus]) < answer.absValue) {
-                answer.absValue = Math.abs(minus[endIndexOfMinus - 1] + minus[endIndexOfMinus]);
-                answer.liquid1 = minus[endIndexOfMinus - 1];
-                answer.liquid2 = minus[endIndexOfMinus];
-            }
-        }
-
-        // 양수 2개 더한 값의 용액값과 이진값에서 구한 용액값 비교
-        if (firstPositive != 0 && secondPositive != 0) {
-            if (Math.abs(firstPositive + secondPositive) < answer.absValue) {
-                answer.absValue = Math.abs(firstPositive + secondPositive);
-                answer.liquid1 = firstPositive;
-                answer.liquid2 = secondPositive;
-            }
-        }
-
-        System.out.printf("%d %d", Math.min(answer.liquid1, answer.liquid2), Math.max(answer.liquid1, answer.liquid2));
-    }
-
-    static class SearchData {
-        long negMin, posMin, positive, negMinValue, posMinValue;
-
-        SearchData (long positive) {
-            this.negMin = Long.MAX_VALUE * -1;
-            this.posMin = Long.MAX_VALUE;
-            this.positive = positive;
-        }
-
-        int eval(int idx) {
-            long mixture = positive + minus[idx];
-            if (mixture < 0 && mixture > negMin) {
-                negMin = mixture;
-                negMinValue = minus[idx];
-                return -1;
-            } else if (mixture > 0 && mixture < posMin) {
-                posMin = mixture;
-                posMinValue = minus[idx];
-                return 1;
-            } else if (mixture == 0) {
-                answer.absValue = 0;
-                answer.liquid1 = positive;
-                answer.liquid2 = minus[idx];
-                return 0;
-            } else {
-                // 예외
-                return 999;
-            }
-        }
-
-        void compareWithGlobal() {
-            // 전체 Binary Min과 비교해서 절대값 더 작으면 덮어씌우기
-            if (Math.abs(negMin) < Math.abs(posMin) && Math.abs(negMin) < answer.absValue) {
-                answer.absValue = Math.abs(negMin);
-                answer.liquid1 = negMinValue;
-                answer.liquid2 = positive;
-            } else if (Math.abs(negMin) >= Math.abs(posMin) && Math.abs(posMin) < answer.absValue) {
-                answer.absValue = Math.abs(posMin);
-                answer.liquid1 = posMinValue;
-                answer.liquid2 = positive;
-            }
-        }
-
-        void printVariables() {
-            System.out.println("[ " + positive + " ] " + "negMin = " + negMin + " negMinValue = " + negMinValue + " posMin = " + posMin + " posMinValue = " + posMinValue);
-        }
-    }
-
-    static boolean binarySearch(long positive) {
-        /**
-         * 하나의 산성용액에 대해 가장 조합이 좋은 염기성용액을 이진탐색을 찾아낸다
-         */
-        
-        // 음수 용액이 없을 경우 이진 탐색 skip 
-        if (endIndexOfMinus < 0) return false;
-
-        SearchData searchData = new SearchData(positive);
-        int left = 0, right = endIndexOfMinus, mid;
-
-        searchLoop:
         while (left < right) {
-            if (left + 1 == right) break;
-            mid = (left + right) / 2;
+            long mix = liquidList[left] + liquidList[right];
+            if (Math.abs(mix) < mixture.valueABS) {
+                mixture.liquidA = liquidList[left];
+                mixture.liquidB = liquidList[right];
+                mixture.valueABS = Math.abs(mix);
+            }
 
-            switch (searchData.eval(mid)) {
-                case -1:
-                    left = mid;
-                    break;
-                case 1:
-                    right = mid;
-                    break;
-                case 0:
-                    return true;
+            // 혼합 용액이 음수일 경우 0에 가까워지려면 합이 더 커져야 한다
+            // 합이 커질려면 index가 더 큰 숫자를 선택해야하므로 left 포인터를 1 증가시킨다
+            // 반대로 합이 작아지려면 index가 더 작은 숫자를 선택해야하므로 right 포인터를 1 감소시킨다
+            if (mix < 0) left++;
+            else if (mix > 0) right--;
+            else {
+                printAnswer(mixture);
+                return;
             }
         }
-        
-        searchData.eval(left);
-        searchData.eval(right);
+        printAnswer(mixture);
+    }
 
-        // negMin과 rightMin 비교
-        searchData.compareWithGlobal();
-
-        return false;
+    static void printAnswer(Mixture mixture) {
+        System.out.printf("%d %d", mixture.liquidA, mixture.liquidB);
     }
 }
