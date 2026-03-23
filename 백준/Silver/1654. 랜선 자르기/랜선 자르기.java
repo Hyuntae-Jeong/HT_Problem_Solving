bdca3a392b542aade7dd50f4da08010f538b6672
Hyuntae <jht000717@gmail.com>
2026-03-23 21:02:34 +0900

    [Silver II] Title: 랜선 자르기, Time: 112 ms, Memory: 15248 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,61 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int K, N;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(token.nextToken());
+        N = Integer.parseInt(token.nextToken());
+
+        System.out.print(findMaxLAN(getInputAndMax()));
+    }
+
+    static long[] lanCables;
+    static long getInputAndMax() throws IOException {
+        lanCables = new long[K];
+        long max = 0;
+
+        for (int i = 0; i < K; i++) {
+            lanCables[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, lanCables[i]);
+        }
+
+        return max;
+    }
+
+    static long findMaxLAN(long startMaxLength) {
+        long L = 1, R = startMaxLength;
+
+        while (L < R) {
+            // 주의: R이 2^31 -1 이라면, L + R은 int 범위를 넘어가게 된다..
+            long lanCount = getAvailableLANcount((L + R) / 2);
+            if (lanCount < N) {
+                R = (L + R) / 2 - 1;
+            } else {
+                L = (L + R) / 2;
+                if (L + 1 == R) {
+                    if (getAvailableLANcount(R) >= N) return R;
+                    else return L;
+                }
+            }
+        }
+
+        return L;
+    }
+
+    static long getAvailableLANcount(long length) {
+        long count = 0;
+        for (int i = 0; i < K; i++) {
+            count += lanCables[i] / length;
+        }
+        return count;
+    }
+}
