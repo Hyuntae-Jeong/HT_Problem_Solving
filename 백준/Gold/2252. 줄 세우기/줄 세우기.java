d53789d6644b4adfb54e5bc6dbbb9d28f503146d
Hyuntae <jht000717@gmail.com>
2026-04-13 22:51:47 +0900

    [Gold III] Title: 줄 세우기, Time: 436 ms, Memory: 51104 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,95 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+class Student {
+    boolean isVisited;
+    Set<Integer> nextStudents;
+    int incomingCount;
+
+    Student() {
+        nextStudents = new HashSet<>();
+    }
+}
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Student[] students;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        students = new Student[N + 1];
+        getInput();
+//        printInput();
+        BFSMethod();
+    }
+
+    static void printInput() {
+        for (int i = 1; i <= N; i++) {
+            System.out.println("=== student " + i + " ===");
+            System.out.println("incoming count: " + students[i].incomingCount);
+            Iterator<Integer> it = students[i].nextStudents.iterator();
+
+            while (it.hasNext()) {
+                System.out.println("next: " + it.next());
+            }
+        }
+    }
+
+    static void getInput() throws IOException {
+        initializeStudents();
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer token = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(token.nextToken());
+            int b = Integer.parseInt(token.nextToken());
+
+            students[a].nextStudents.add(b);
+            students[b].incomingCount++;
+        }
+    }
+
+    static void initializeStudents() {
+        for (int i = 1; i <= N; i++) {
+            students[i] = new Student();
+        }
+    }
+
+    static void BFSMethod() {
+        StringBuilder sb = new StringBuilder();
+        int answerCount = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        while (answerCount < N) {
+            for (int i = 1; i <= N; i++) {
+                if (students[i].incomingCount == 0) {
+                    queue.add(i);
+                }
+            }
+
+            while (!queue.isEmpty()) {
+                int student = queue.poll();
+                sb.append(student).append(" ");
+                answerCount++;
+
+                for (Integer next : students[student].nextStudents) {
+                    if (--students[next].incomingCount == 0) {
+                        queue.add(next);
+                    }
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    static void DFSMethod() {}
+}
